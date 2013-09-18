06f953b58e79b852fe7d83e5556a93f4a00a3a24
Daraciel <daraciel@gmail.com>
2013-09-18 14:09:15 +0200

    Soporte multidioma iniciado
    Hecha la prueba de la carga desde el strings.resx.
    
    Strings.es.resx está entero
    Strings.ru.resx esta creado pero no empezado

--- JanPiedras2/JanPiedras2/Site.Master.cs
+++ JanPiedras2/JanPiedras2/Site.Master.cs
@@ -4,13 +4,52 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Threading;
+using System.Globalization;
+using System.Resources;
+using System.Reflection;
 
 namespace JanPiedras2
 {
     public partial class SiteMaster : System.Web.UI.MasterPage
     {
+        ResourceManager rm;
+        CultureInfo ci;
+        private void LoadString(CultureInfo ci)
+        {
+            lblMateriales.Text = rm.GetString("Materiales", ci);
+
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                Thread.CurrentThread.CurrentCulture = new CultureInfo("es");
+                rm = new ResourceManager("Resources.Strings",
+                         System.Reflection.Assembly.Load("App_GlobalResources"));
+                ci = Thread.CurrentThread.CurrentCulture;
+                LoadString(ci);
+            }
+            else
+            {
+                rm = new ResourceManager("Resources.Strings",
+                         System.Reflection.Assembly.Load("App_GlobalResources"));
+                ci = Thread.CurrentThread.CurrentCulture;
+                LoadString(ci);
+            }
+        }
+
+        protected void btnRU_Click(object sender, ImageClickEventArgs e)
+        {
+
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("RU");
+            LoadString(Thread.CurrentThread.CurrentCulture);
+        }
+
+        protected void btnESP_Click(object sender, ImageClickEventArgs e)
+        {
+
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("es");
+            LoadString(Thread.CurrentThread.CurrentCulture);
         }
 
     }
--- JanPiedras2/JanPiedras2/Site.Master.designer.cs
+++ JanPiedras2/JanPiedras2/Site.Master.designer.cs
@@ -31,6 +31,33 @@ namespace JanPiedras2 {
         protected global::System.Web.UI.ScriptManager ScriptManager1;
         
         /// <summary>
+        /// Control lblMateriales.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMateriales;
+        
+        /// <summary>
+        /// Control btnESP.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton btnESP;
+        
+        /// <summary>
+        /// Control btnRU.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton btnRU;
+        
+        /// <summary>
         /// Control MainContent.
         /// </summary>
         /// <remarks>
